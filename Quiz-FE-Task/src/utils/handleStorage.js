fc410d329f7519d86675c5129852226bcd1f0539
salah856 <salah_elhossiny856@yahoo.com>
2021-10-29 15:24:24 +0200

    handle local storage

--- Quiz-FE-Task/src/utils/handleStorage.js
+++ Quiz-FE-Task/src/utils/handleStorage.js
@@ -1,6 +1,7 @@
 
 const handleLocalStorage = (quiz) =>{
-        
+    
+    console.log(quiz)
     let quizzes = JSON.parse(localStorage.getItem('quizzes')); 
     quizzes.push(quiz); 
     localStorage.setItem('quizzes', JSON.stringify(quizzes)); 
