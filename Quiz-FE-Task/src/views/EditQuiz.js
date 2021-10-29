d3d8f278bc8b07df80e221fc9a5af30623bed471
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-29 13:32:47 +0200

    edit quiz

--- Quiz-FE-Task/src/views/EditQuiz.js
+++ Quiz-FE-Task/src/views/EditQuiz.js
@@ -1,10 +1,16 @@
 
 import {useParams} from 'react-router-dom'; 
+import {Quiz} from 'react-quizzes'; 
 
 
 const EditQuiz=()=>{
 
     const {id} = useParams(); 
+
+    // to do get quiz from local storage by id value; 
+
+    // then handle properly the format of data required for quiz component from react-quizzes packages; 
+
 
 
     return (
