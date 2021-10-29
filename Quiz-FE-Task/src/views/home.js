c78a5da28fdc3e313bee6358d2a2c78cc9f2bec8
salah856 <salah_elhossiny856@yahoo.com>
2021-10-29 12:20:01 +0200

    refactor list quizzes

--- Quiz-FE-Task/src/views/home.js
+++ Quiz-FE-Task/src/views/home.js
@@ -1,39 +1,17 @@
 import React, {useState, useEffect} from "react";
-import axios from 'axios'; 
 import { useHistory } from "react-router";
 import MUIDataTable from "mui-datatables";
 
 
 function Home(){
 
-  const [topics, setTopics] = useState([]);
   const history = useHistory();
     
     useEffect( () => { 
-        async function fetchTopics() {
-            try {
-                const res = await axios.get(
-                  'https://616d7fff6dacbb001794cacb.mockapi.io/api/v1/topics'
-                  ); 
-                setTopics(res.data);
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        fetchTopics();
+       
     }, []);
 
-  const columns = [
-    'createdAt', 
-    'name', 
-    'avatar', 
-    'id'
-  ]; 
-
-  const options =  {
-    filterType: 'dropdown', 
-    responsive: 'scroll'
-  }; 
+    
 
 
   return (
@@ -48,12 +26,7 @@ function Home(){
     >
       Create Quiz
     </button>
-    <MUIDataTable 
-      title={"Topics"} 
-      data={topics} 
-      columns={columns} 
-      options={options} 
-    />
+
     </>
   )
 };
