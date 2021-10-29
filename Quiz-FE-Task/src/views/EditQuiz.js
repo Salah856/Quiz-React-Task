ff0812d0384d7bdbb573f11159b5af77e684e8b0
salah856 <salah_elhossiny856@yahoo.com>
2021-10-29 17:08:47 +0200

    handle edit quiz submit

--- Quiz-FE-Task/src/views/EditQuiz.js
+++ Quiz-FE-Task/src/views/EditQuiz.js
@@ -4,10 +4,31 @@ import { Button, TextField } from '@material-ui/core';
 
 const EditQuiz = () => {
 
-    const { id } = useParams(); 
+    const { id } = useParams();
+    const [title, setTitle] = useState(''); 
+    const [description, setDescription] = useState(''); 
+    const [URL, setURL] = useState('');  
 
     let quiz = getQuiz(id); 
     console.log(quiz); 
+
+   
+    const handleTitleChange=(e)=>{
+        setTitle(e.target.value); 
+    }; 
+    
+    const handleDescriptionChange=(e)=>{
+        setDescription(e.target.value); 
+    }; 
+
+    const handleURLChange=(e)=>{
+        setURL(e.target.value); 
+    }; 
+
+    const handleEditQuizSubmit = () => {
+       
+        // to do 
+    }
 
     return (
 
@@ -16,18 +37,21 @@ const EditQuiz = () => {
                 label="Title"
                 value={quiz.title}
                 style={{margin: '40px'}} 
+                onChange={handleTitleChange}
             />
 
             <TextField 
                 label="Description"
                 value={quiz.description}
                 style={{margin: '40px'}} 
+                onChange={handleDescriptionChange}
             />
 
             <TextField 
                 label="URL"
                 value={quiz.URL}
                 style={{margin: '40px'}}
+                onChange={handleURLChange}
             />
 
 
