75662619705595a154a5f34812fcd70aee6218a8
salah856 <salah_elhossiny856@yahoo.com>
2021-10-29 17:12:04 +0200

    edit handle click on edit submit

--- Quiz-FE-Task/src/views/EditQuiz.js
+++ Quiz-FE-Task/src/views/EditQuiz.js
@@ -65,7 +65,7 @@ const EditQuiz = () => {
                     color: 'white', 
                     backgroundColor: 'blue'
                 }}
-                //onClick={}
+                onClick={handleEditQuizSubmit}
             >
                 Edit Quiz
             </Button>
